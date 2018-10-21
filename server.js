64a04520e4016863ab45767ec920d0f89aab6070
patrick <patrick.kurzeja@gmail.com>
2018-10-21 17:00:48 +0200

    Initial commit

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 
 const express = require('express');
+const expressEdge = require('express-edge');
 
-
-const app = new express();
-
+const app = express();
 
 
 app.use(express.static('public'));
+
+app.use(expressEdge);
+
+
 app.set('views', `${__dirname}/views`);
-app.set('view engine', 'pug');
+app.set('view engine', 'edge');
+
+app.get('/', (req, res) => {
+    res.render('index',  { title: 'Hey', message: 'Hello there!' });
+});
 
 app.listen(3000, () => {
     console.log('App listening on port 3000!');
+    if (process.send) {
+        process.send('online')
+        
+    }
 });
